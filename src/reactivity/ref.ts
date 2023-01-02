969a01228024740a5cb26baa99dd7cf7c125636c
pika <1040906259@qq.com>
2023-01-02 20:55:51 +0800

    feat: ref响应式变量

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,4 +1,6 @@
 import { isTracking, trackEffects, triggerEffects } from './effect'
+import reactive from './reactive'
+import { isObject } from 'lodash'
 
 /**
  * ref 接受一个内部值，返回一个响应式的、可更改的 ref 对象，此对象只有一个指向其内部值的属性 .value。
@@ -7,10 +9,14 @@ class RefImpl {
   // 保存内部值
   private _value: any
   // 依赖收集
-  public dep
+  public dep: Set<Function>
+  // ref的真值
+  private _rewValue: any
 
   constructor(value) {
-    this._value = value
+    this._rewValue = value
+    // 如果是对象，转换为响应式对象
+    this._value = convertReactiveObject(value)
     this.dep = new Set()
   }
   get value() {
@@ -20,9 +26,9 @@ class RefImpl {
   }
   set value(newValue) {
     // 如果值没有变化，不做处理
-    if (Object.is(this._value, newValue)) return
+    if (Object.is(this._rewValue, newValue)) return
     // 更新内部值
-    this._value = newValue
+    this._value = convertReactiveObject(newValue)
     triggerEffects(this.dep)
   }
 }
@@ -35,3 +41,7 @@ const trackRefValue = ref => {
     trackEffects(ref.dep)
   }
 }
+
+// 转化响应式对象
+const convertReactiveObject = value =>
+  isObject(value) ? reactive(value) : value
--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -23,4 +23,17 @@ describe('ref', () => {
     expect(calls).toBe(2)
     expect(dummy).toBe(2)
   })
+
+  it('响应式嵌套对象', () => {
+    const a = ref({
+      count: 1,
+    })
+    let dummy
+    effect(() => {
+      dummy = a.value.count
+    })
+    expect(dummy).toBe(1)
+    a.value.count = 2
+    expect(dummy).toBe(2)
+  })
 })
